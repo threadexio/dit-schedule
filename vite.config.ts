43fa2b5ebbc554d4b7010da6b8d5cac7a86f4c45
threadexio <pzarganitis@gmail.com>
2025-10-11 10:37:35 +0300

    enable https for vite
    ical-generator uses crypto.* which is only available
    on https or localhost. This fails if accessed from
    outside localhost if vite is started with --host.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+import fs from 'fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync('tools/vite/ssl-key.pem'),
+      cert: fs.readFileSync('tools/vite/ssl-cert.pem'),
     },
   },
 })
